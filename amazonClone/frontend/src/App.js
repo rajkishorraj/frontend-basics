55a4aec2e2eb185a5d0a7541d1bf36f80a6ac417
Raj Kishor <raj.kishor@oracle.com>
2022-07-25 23:34:33 +0530

    adding product screen

--- amazonClone/frontend/src/App.js
+++ amazonClone/frontend/src/App.js
@@ -1,57 +1,33 @@
-import React from 'react';
-import data from './data';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import HomeScreen from "./screen/HomeScreen";
+import ProductScreen from "./screen/ProductScreen";
 
 function App() {
   return (
-    <div className="grid-container">
-      <header className="row">
-        <div>
-          <a className="brand" href="/">Amazon</a>
-        </div>
-        <div>
-          <a href="/cart">Cart</a>
-          <a href="/sigin">Sign In</a>
-        </div>
-      </header>
-
-      <main>
-        <div className="row center">
-          {
-            data.products.map(product => (
-            <div key={product._id} className="card">
-              <a href={`/product/${product._id}`}>
-                <img className="medium" src={product.image} alt={product.name} />
-              </a>
-            <div className="card-body">
-              <a href={`/product/${product._id}`}>
-                <h2>{product.name}</h2>
-              </a>
-              <div className="rating">
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-                <span>
-                  <i className="fa fa-star"></i>
-                </span>
-              </div>
-              <div className="price">${product.price}</div>
-            </div>
+    <BrowserRouter>
+      <div className="grid-container">
+        <header className="row">
+          <div>
+            <a className="brand" href="/">
+              Amazon
+            </a>
           </div>
-            ))
-          }
-        </div>
-      </main>
-      <footer className="row center">All Rights reserved</footer>
-    </div>
+          <div>
+            <a href="/cart">Cart</a>
+            <a href="/sigin">Sign In</a>
+          </div>
+        </header>
+
+        <main>
+          <Routes>
+            <Route path="product/:id" element={<ProductScreen />}></Route>
+            <Route path="/" element={<HomeScreen />} exact></Route>
+          </Routes>
+        </main>
+        <footer className="row center">All Rights reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
